b562cdc7e53ad1f75be3ca0f8ad32a511ad30b8b
Kel <55993339+jg39gao@users.noreply.github.com>
2023-01-30 14:44:26 +0800

    Update Py_note.py
    add counter

--- Py_note.py
+++ Py_note.py
@@ -226,6 +226,20 @@ print('Pandas version ' + pd.__version__)
 
 
 ==================================
+Notes @Shanghai Python #Jan 2023
+==================================
+
+Counter 排序
+>>> from collections import Counter
+>>> counter = Counter({'A': 10, 'C': 5, 'H': 7})
+>>> counter.most_common()
+[('A', 10), ('H', 7), ('C', 5)]
+>>> sorted(counter.items())
+[('A', 10), ('C', 5), ('H', 7)]
+
+
+
+==================================
 Notes @Diamond Python #Oct 2019
 ==================================
 
