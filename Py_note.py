a2190e8f4cf26b7fc93b6a02b04793ae36123f85
Kel <55993339+jg39gao@users.noreply.github.com>
2023-03-28 12:34:20 +0800

    Update Py_note.py

--- Py_note.py
+++ Py_note.py
@@ -533,6 +533,15 @@ if match:
 m = re.match(r"(\w+) [a-z]{2} (\w+)", "Isaac xx Newton, physicist")
 m.groups() #('Isaac', 'Newton')  only return groups.
 m.group(0) # 'Isaac xx Newton'
+
+# 注意match()和findall()的区别，match是看给的text是不是匹配，只要有一处匹配就返回true，findall是在给的text中找出所有符合的字符串出来。 
+
+m = re.findall(r"(classic solitaire[0-9]?)", 'Classic Solitaire (CS,S3,纸牌3), Classic Solitaire1  Classic Solitaire2 ' 
+             ,flags= re.IGNORECASE
+             )
+
+print('true' if m else 'false')
+list(m) # ['Classic Solitaire', 'Classic Solitaire1', 'Classic Solitaire2']
 # ************************************************************
 # set() 
 s1={1,2,3,4,'a','b','c'}
